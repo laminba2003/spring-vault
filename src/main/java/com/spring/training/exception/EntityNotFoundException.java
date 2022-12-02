72c5e79132f8b54ce4ea5efc2d933e110b82b5dd
mamadou lamine ba <laminba2003@gmail.com>
2022-12-02 21:50:56 +0000

    improvements

--- src/main/java/com/spring/training/exception/EntityNotFoundException.java
+++ src/main/java/com/spring/training/exception/EntityNotFoundException.java
@@ -1,14 +1,14 @@
 package com.spring.training.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class EntityNotFoundException extends RuntimeException {
-    String message;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
 }
